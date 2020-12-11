50bb64f8c33f72e5168ffc9282df2546c4478ea6
Luís Cunha <luiscvnha@gmail.com>
2020-12-11 21:08:03 +0000

    esqueleto do api-server

---
+++ api-server/app.js
@@ -0,0 +1,45 @@
+const createError = require('http-errors')
+const express = require('express')
+const logger = require('morgan')
+const mongoose = require('mongoose')
+const cors = require('cors')
+
+const mongoDB = 'mongodb://127.0.0.1/RepositoriDOIS'
+mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false})
+
+const db = mongoose.connection
+db.on('error', function() {
+    console.log('Error connecting to MongoDB...')
+})
+db.once('open', function() {
+    console.log('Connected to MongoDB...')
+})
+
+const indexRouter = require('./routes/index')
+
+var app = express()
+
+app.use(cors())
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.use('/', indexRouter)
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    next(createError(404))
+})
+
+// error handler
+app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message
+    res.locals.error = req.app.get('env') === 'development' ? err : {}
+
+    // render the error page
+    res.status(err.status || 500)
+    res.jsonp('Error: path not found on this server...')
+})
+
+module.exports = app

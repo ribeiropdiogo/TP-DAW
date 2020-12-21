9d585fcd4e9bd0b9063341670947c4f7825bbdf9
Luís Cunha <luiscvnha@gmail.com>
2020-12-21 21:11:19 +0000

    fix e documentação

--- api-server/controllers/utilizador.js
+++ api-server/controllers/utilizador.js
@@ -1,20 +1,30 @@
 // User Controller
 const Utilizador = require('../models/utilizador')
 
+
 // Returns User list
 module.exports.list = function() {
-    return Utilizador.find({}, {hashedPassword: 0, salt: 0, __v: 0}).exec()
+    return Utilizador.find(
+        {},
+        {hashedPassword: 0, salt: 0, __v: 0}
+    ).exec()
 }
+
 
 // Returns a user record
 module.exports.lookup = function(id) {
-    return Utilizador.findOne({username: id}, {hashedPassword: 0, salt: 0, __v: 0}).exec()
+    return Utilizador.findOne(
+        {username: id},
+        {hashedPassword: 0, salt: 0, __v: 0}
+    ).exec()
 }
+
 
 // Returns a user login credentials
 module.exports.credentials = function(id) {
     return Utilizador.findOne({username: id}, {__v: 0}).exec()
 }
+
 
 // Inserts a new user
 module.exports.insert = function(u) {
@@ -22,12 +32,14 @@ module.exports.insert = function(u) {
     return novoUtilizador.save()
 }
 
-// Removes one user
-module.exports.remove = function(id) {
-    return Utilizador.deleteOne({username: id}).exec()
-}
 
 // Edit one user
 module.exports.edit = function(id, u) {
     return Utilizador.findByIdAndUpdate(id, u, {new: true}).exec()
+}
+
+
+// Removes one user
+module.exports.remove = function(id) {
+    return Utilizador.deleteOne({username: id}).exec()
 }
--- api-server/routes/utilizador.js
+++ api-server/routes/utilizador.js
@@ -5,7 +5,7 @@ const crypto = require('../util/crypto')
 const Utilizador = require('../controllers/utilizador')
 
 
-function filter(user) {
+function filter_send(user) {
     user.hashedPassword = undefined
     user.salt = undefined
     user.__v = undefined
@@ -20,6 +20,7 @@ router.get('/', function(req, res) {
         .catch(error => res.status(500).jsonp(error))
 })
 
+
 // GET /utilizadores/:id
 router.get('/:id', function(req, res) {
     Utilizador.lookup(req.params.id)
@@ -27,11 +28,14 @@ router.get('/:id', function(req, res) {
         .catch(error => res.status(500).jsonp(error))
 })
 
+
 // POST /utilizadores
 router.post('/', function(req, res) {
     const user = req.body
 
     user.admin = false
+    user.dataRegisto = new Date()
+    user.ultimoAcesso = user.dataRegisto
 
     var hash = crypto.hasher(user.pass, crypto.generateSalt())
     user.hashedPassword = hash.hashedPassword
@@ -39,35 +43,38 @@ router.post('/', function(req, res) {
     user.pass = undefined
 
     Utilizador.insert(user)
-        .then(data => res.status(201).jsonp(filter(data)))
+        .then(data => res.status(201).jsonp(filter_send(data)))
         .catch(error => res.status(500).jsonp(error))
 })
+
 
 // POST /utilizadores/login
 router.post('/login', function(req, res) {
     Utilizador.credentials(req.body.username)
         .then(data => {
-            if (crypto.compare(req.body.pass, data)) {
-                res.status(200).send()
-            } else {
+            if (crypto.compare(req.body.pass, data))
+                res.status(200).jsonp(filter_send(data))
+            else
                 res.status(401).send()
-            }
         })
         .catch(error => res.status(500).jsonp(error))
 })
 
+
 // PUT /utilizadores/:id
 router.put('/:id', function(req, res) {
     Utilizador.edit(req.params.id, req.body)
-        .then(data => res.status(200).jsonp(filter(data)))
+        .then(data => res.status(200).jsonp(filter_send(data)))
         .catch(error => res.status(500).jsonp(error))
 })
+
 
 // DELETE /utilizadores/:id
 router.delete('/:id', function(req, res) {
     Utilizador.remove(req.params.id)
-        .then(data => res.status(200).jsonp(filter(data)))
+        .then(data => res.status(200).jsonp(filter_send(data)))
         .catch(error => res.status(500).jsonp(error))
 })
+
 
 module.exports = router
